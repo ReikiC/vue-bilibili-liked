ffced62a63e77ae1d7fdf759702ecf151c9a91ea
Dankao Chen <116425012+Reikimen@users.noreply.github.com>
2025-05-04 15:45:49 +0100

    test: basic usage of vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <hi> hi {{ msg }} </hi>
+    <!-- <div @click="add">{{ count }}</div> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <input type="text" v-model="val" @keypress.enter="addTodo" />
+    <button @click="addTodo">add</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.title">
+        <input type="checkbox" v-model="todo.done" />
+        <span>
+          {{ todo.title }}
+        </span>
+      </li>
+    </ul>
+    <div>
+      {{ doneCount }} /
+      {{ todos.length }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      msg: 'Wellcome to Your Vue.js App',
+      count: 1,
+      val: '',
+      todos: [
+        { title: 'eat', done: true },
+        { title: 'sleep', done: false },
+        { title: 'code', done: true },
+      ],
+    }
+  },
+  computed: {
+    doneCount() {
+      return this.todos.filter((v) => v.done).length
+    },
+  },
+  methods: {
+    add() {
+      this.count++
+    },
+    addTodo() {
+      this.todos.push({
+        title: this.val,
+        done: false,
+      })
+      this.val = ''
+    },
+  },
+})
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
